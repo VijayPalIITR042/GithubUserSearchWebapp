8fd3fa9afdbd96a285d917b8a3f965239fccf423
VijayPalIITR042 <vijaypaliitrrrp@gmail.com>
2017-01-24 21:31:18 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,99 @@
+jQuery(document).ready(function(){
+	//cache DOM elements
+	
+
+	//on resize, move search and top nav position according to window width
+	var resizing = false;
+	$(window).on('resize', function(){
+		if( !resizing ) {
+			(!window.requestAnimationFrame) ? setTimeout(moveNavigation, 300) : window.requestAnimationFrame(moveNavigation);
+			resizing = true;
+		}
+	});
+
+	//on window scrolling - fix sidebar nav
+	var scrolling = false;
+	
+	$(window).on('scroll', function(){
+		if( !scrolling ) {
+			(!window.requestAnimationFrame) ? setTimeout(checkScrollbarPosition, 300) : window.requestAnimationFrame(checkScrollbarPosition);
+			scrolling = true;
+		}
+	});
+
+	
+	
+	
+	 $('#searchUser').on('keyup', function(e){
+    let username = e.target.value;
+
+    // Make request to Github
+    $.ajax({
+      url:'https://api.github.com/users/'+username,
+      data:{
+        client_id:'437e05dfb5b46526ec21',
+        client_secret:'e473dbb5b27c0b2ffae5c1f984d8dfce0d44c84f'
+      }
+    }).done(function(user){
+      $.ajax({
+        url:'https://api.github.com/users/'+username+'/repos',
+        data:{
+          client_id:'437e05dfb5b46526ec21',
+          client_secret:'e473dbb5b27c0b2ffae5c1f984d8dfce0d44c84f',
+          sort: 'created: asc',
+          per_page: 10
+        }
+      }).done(function(repos){
+        $.each(repos, function(index, repo){
+          $('#repos').append(`
+            <div class="well">
+              <div class="row">
+                <div class="col-md-7">
+                  <strong>${repo.name}</strong>: ${repo.description}
+                </div>
+                <div class="col-md-3">
+                  <span class="label label-default">Forks: ${repo.forks_count}</span>
+                  <span class="label label-primary">Watchers: ${repo.watchers_count}</span>
+                  <span class="label label-success">Stars: ${repo.stargazers_count}</span>
+                </div>
+                <div class="col-md-2">
+                  <a href="${repo.html_url}" target="_blank" class="btn btn-default">Repo Page</a>
+                </div>
+              </div>
+            </div>
+          `);
+        });
+      });
+      $('#profile').html(`
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">${user.name}</h3>
+          </div>
+          <div class="panel-body">
+            <div class="row">
+              <div class="col-md-3">
+                <img class="thumbnail img-responsive avatar" src="${user.avatar_url}">
+                <a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}">View Profile</a>
+              </div>
+              <div class="col-md-9">
+              <span class="label label-default">Public Repos: ${user.public_repos}</span>
+              <span class="label label-primary">Public Gists: ${user.public_gists}</span>
+              <span class="label label-success">Followers: ${user.followers}</span>
+              <span class="label label-info">Following: ${user.following}</span>
+              <br><br>
+              <ul class="list-group">
+                <li class="list-group-item">Company: ${user.company}</li>
+                <li class="list-group-item">Website/blog: ${user.blog}</li>
+                <li class="list-group-item">Location: ${user.location}</li>
+                <li class="list-group-item">Member Since: ${user.created_at}</li>
+              </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h3 class="page-header">Latest Repos</h3>
+        <div id="repos"></div>
+      `);
+    });
+  });
+});
